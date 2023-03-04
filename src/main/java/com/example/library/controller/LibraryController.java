44cf3587f697165e262ac48a8c532a60d5f935ac
Milosz <milosz.sierakowski@wp.pl>
2023-03-04 15:06:06 +0100

    0.1.1 update configuration

---
+++ src/main/java/com/example/library/controller/LibraryController.java
@@ -0,0 +1,18 @@
+package com.example.library.controller;
+
+public class LibraryController {
+//todo    dodanie czytelnika
+
+//todo    dodanie tytułu
+
+//todo    dodanie egzemplarza
+
+//todo    zmiana statusu egzemplarza
+
+//todo    sprawdzenie ilości egzemplarzy danego tytułu dostępnych do wypożyczenia
+
+//todo    wypożyczenie książki
+
+//todo    zwrot książki
+
+}
