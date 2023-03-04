77d2a709cd061ae62fa34fac979a20a909f3d67c
Milosz <milosz.sierakowski@wp.pl>
2023-03-04 16:52:23 +0100

    0.2.4 prepare ReaderMapper

--- src/main/java/com/example/library/mapper/ReaderMapper.java
+++ src/main/java/com/example/library/mapper/ReaderMapper.java
@@ -1,4 +1,18 @@
 package com.example.library.mapper;
 
+import com.example.library.domain.Reader;
+import com.example.library.domain.ReaderDto;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ReaderMapper {
+    public Reader mapToReader(ReaderDto readerDto) {
+        return new Reader(readerDto.getId(), readerDto.getFirstname(),
+                readerDto.getLastname(), readerDto.getRegistrationDate());
+    }
+
+    public ReaderDto mapToBookDto(Reader reader) {
+        return new ReaderDto(reader.getId(), reader.getFirstname(),
+                reader.getLastname(), reader.getRegistrationDate());
+    }
 }
---
+++ src/test/java/com/example/library/mapper/ReaderMapperTest.java
@@ -0,0 +1,16 @@
+package com.example.library.mapper;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReaderMapperTest {
+
+    @Test
+    void mapToReader() {
+    }
+
+    @Test
+    void mapToBookDto() {
+    }
+}
