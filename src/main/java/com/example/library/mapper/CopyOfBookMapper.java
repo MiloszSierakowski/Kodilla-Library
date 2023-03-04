d3f815ac7711bda148868aa633f2d4a83bcecc8e
Milosz <milosz.sierakowski@wp.pl>
2023-03-04 16:49:22 +0100

    0.2.3 prepare CopyOfBookMapper

--- src/main/java/com/example/library/mapper/CopyOfBookMapper.java
+++ src/main/java/com/example/library/mapper/CopyOfBookMapper.java
@@ -1,4 +1,18 @@
 package com.example.library.mapper;
 
+import com.example.library.domain.CopyOfBook;
+import com.example.library.domain.CopyOfBookDto;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CopyOfBookMapper {
+    public CopyOfBook mapToCopyOfBook(CopyOfBookDto copyOfBookDto) {
+        return new CopyOfBook(copyOfBookDto.getId(), copyOfBookDto.getBookId(),
+                copyOfBookDto.getStatus(), copyOfBookDto.isRental());
+    }
+
+    public CopyOfBookDto mapToCopyOfBookDto(CopyOfBook copyOfBook) {
+        return new CopyOfBookDto(copyOfBook.getId(), copyOfBook.getBookId(),
+                copyOfBook.getStatus(), copyOfBook.isRental());
+    }
 }
---
+++ src/test/java/com/example/library/mapper/CopyOfBookMapperTest.java
@@ -0,0 +1,16 @@
+package com.example.library.mapper;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CopyOfBookMapperTest {
+
+    @Test
+    void mapToCopyOfBook() {
+    }
+
+    @Test
+    void mapToCopyOfBookDto() {
+    }
+}
