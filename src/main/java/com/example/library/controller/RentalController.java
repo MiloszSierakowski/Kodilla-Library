26fc721afb96e7a3ab8edf895f1721a33b9ad6b2
Milosz <milosz.sierakowski@wp.pl>
2023-03-11 20:14:22 +0100

    0.9 Finish adding endpoints start working on exceptions

--- src/main/java/com/example/library/controller/RentalController.java
+++ src/main/java/com/example/library/controller/RentalController.java
@@ -1,18 +1,55 @@
 package com.example.library.controller;
 
+import com.example.library.domain.CopyOfBook;
+import com.example.library.domain.Rental;
+import com.example.library.domain.RentalDto;
+import com.example.library.mapper.RentalMapper;
+import com.example.library.service.CopyOfBookService;
+import com.example.library.service.RentalService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/v1/rental")
 @RequiredArgsConstructor
 @CrossOrigin("*")
 public class RentalController {
+    private final RentalMapper rentalMapper;
+    private final RentalService rentalService;
+    private final CopyOfBookService copyOfBookService;
 
-    //todo    wypożyczenie książki
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> createNewRental(@RequestBody RentalDto rentalDto) {
+        Rental rental = rentalMapper.mapToRental(rentalDto);
 
-    //todo    zwrot książki
+        Optional<CopyOfBook> copyOfBook = copyOfBookService.findById(rental.getCopyOfBook().getId());
 
+        if (copyOfBook.isPresent()) {
+            rentalService.saveNewRental(rental);
+            copyOfBook.get().setRental(true);
+            copyOfBookService.saveCopyOfBook(copyOfBook.get());
+        } else {
+            //todo exeption
+        }
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> finishRental(@RequestBody RentalDto rentalDto) {
+        Optional<Rental> rental = rentalService.findById(rentalDto.getId());
+        Optional<CopyOfBook> copyOfBook = copyOfBookService.findById(rentalDto.getCopyOfBook().getId());
+
+        if (rental.isPresent() && copyOfBook.isPresent()) {
+            rental.get().setReturnDate(rentalDto.getReturnDate());
+            copyOfBook.get().setRental(false);
+        } else {
+            //todo exeption
+        }
+
+        return ResponseEntity.ok().build();
+    }
 }
--- src/main/java/com/example/library/service/RentalService.java
+++ src/main/java/com/example/library/service/RentalService.java
@@ -1,11 +1,22 @@
 package com.example.library.service;
 
+import com.example.library.domain.Rental;
 import com.example.library.repository.RentalRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class RentalService {
     private final RentalRepository rentalRepository;
+
+    public Rental saveNewRental(Rental rental) {
+        return rentalRepository.save(rental);
+    }
+
+    public Optional<Rental> findById(Long id){
+        return rentalRepository.findById(id);
+    }
 }
